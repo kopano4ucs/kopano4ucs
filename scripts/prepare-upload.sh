ff48f45b3aeec412b71400accb6e7c680b4e4344
Felix Bartels <felix@host-consultants.de>
2016-07-08 19:00:47 +0200

    make the script more generic, upload to kopano owncloud instead of creating a tar.gz

--- scripts/prepare-upload.sh
+++ scripts/prepare-upload.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 ## Script makes use of the open build service cli client and a private buildnode. Therefore it only works from the Kopano offices.
 
-tempdir=$(mktemp -d)
+## TODO: copy stuff from the core download into subdir for webapp app
 
-mkdir -p $tempdir/{kopano4ucs,core,webapp,webmeetings,z-push}
+osc_get () {
+	arch="x86_64"
+	distro="Univention_4.0"
+	project="$1"
+	package="$2"
+	out="$3"
+	mkdir -p  $out
+	echo "$package - $out"
+	osc getbinaries $project $package $distro $arch -d $out
+}
+
+outdir="$HOME/ownCloud-Kopano/kopano4ucs"
 
 # kopano4ucs
-osc getbinaries kopano4ucs kopano4ucs Univention_4.0 x86_64 -d $tempdir/kopano4ucs
+osc_get kopano4ucs kopano4ucs $outdir/kopano4ucs
+osc_get kopano4ucs meetings4ucs $outdir/webmeetings
+osc_get kopano4ucs mod_proxy_wstunnel $outdir/webmeetings
 
 # core
-osc getbinaries core:pre-final kopano Univention_4.0 x86_64 -d $tempdir/core
+osc_get core:pre-final kopano $outdir/core
 
 # webapp
-osc getbinaries webapp:final kopano-webapp Univention_4.0 x86_64 -d $tempdir/webapp
+osc_get webapp:final kopano-webapp $outdir/webapp
 
 # webmeetings
-osc getbinaries webmeetings:final kopano-webmeetings Univention_4.0 x86_64 -d $tempdir/webmeetings
-osc getbinaries webmeetings:final kopano-webapp-plugin-meetings Univention_4.0 x86_64 -d $tempdir/webmeetings
+osc_get webmeetings:final kopano-webmeetings $outdir/webmeetings
+osc_get webmeetings:final kopano-webapp-plugin-meetings $outdir/webmeetings
 
 # z-push
-osc getbinaries z-push:pre-final z-push Univention_4.0 x86_64 -d $tempdir/z-push
+osc_get z-push:pre-final z-push $outdir/z-push
 
 # remove source and other files
-find $tempdir -type f ! -name "*.deb" -delete
-
-tar czvf univention-kopano-release.tar.gz $tempdir
-
-rm -r $tempdir
+find $outdir -type f ! -name "*.deb" -delete
