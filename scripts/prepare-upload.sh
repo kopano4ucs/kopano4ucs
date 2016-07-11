50d57dde03b6bb228e3bfd651acfd789be430b24
Felix Bartels <felix@host-consultants.de>
2016-07-11 21:30:37 +0200

    refractor script to copy files into app specific folders and tar the result

--- scripts/prepare-upload.sh
+++ scripts/prepare-upload.sh
@@ -21,46 +21,68 @@ outdir="$HOME/ownCloud-Kopano/kopano4ucs"
 osc_get kopano4ucs kopano4ucs $outdir/kopano4ucs
 
 # core
-osc_get core:pre-final kopano $outdir/core
+osc_get core:pre-final kopano $outdir/kopano-core/packages
+
+# copying over kopano4ucs dependencies
+deps="kopano4ucs-lib kopano4ucs-multiserver kopano4ucs-schema kopano4ucs-udm kopano4ucs"
+for pkg in $deps; do
+  cp $outdir/kopano4ucs/"$pkg"*.deb $outdir/kopano-webapp/packages/
+done
 
 # webapp
-osc_get webapp:final kopano-webapp $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-delayeddelivery $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-desktopnotifications $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-filepreviewer $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-de-at $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-de-ch $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-de-de $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-en-gb $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-en-us $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-es-es $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-fr-fr $outdir/webapp
-osc_get webapp:final kopano-webapp-plugin-spell-nl-nl $outdir/webapp
+osc_get webapp:final kopano-webapp $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-delayeddelivery $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-desktopnotifications $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-filepreviewer $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-de-at $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-de-ch $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-de-de $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-en-gb $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-en-us $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-es-es $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-fr-fr $outdir/kopano-webapp/packages
+osc_get webapp:final kopano-webapp-plugin-spell-nl-nl $outdir/kopano-webapp/packages
 
-osc_get files:final kopano-webapp-plugin-files $outdir/webapp
-osc_get files:final kopano-webapp-plugin-filesbackend-owncloud $outdir/webapp
-osc_get files:final kopano-webapp-plugin-filesbackend-smb $outdir/webapp
+osc_get files:final kopano-webapp-plugin-files $outdir/kopano-webapp/packages
+osc_get files:final kopano-webapp-plugin-filesbackend-owncloud $outdir/kopano-webapp/packages
+osc_get files:final kopano-webapp-plugin-filesbackend-smb $outdir/kopano-webapp/packages
 
-osc_get mdm:final kopano-webapp-plugin-mdm $outdir/webapp
+osc_get mdm:final kopano-webapp-plugin-mdm $outdir/kopano-webapp/packages
 
-osc_get smime:final kopano-webapp-plugin-smime $outdir/webapp
+osc_get smime:final kopano-webapp-plugin-smime $outdir/kopano-webapp/packages
 
-deps="php5-mapi"
-echo "webapp dependencies - $outdir/webapp-dependencies"
-mkdir -p $outdir/webapp-dependencies
+# copying over dependencies from kopano core
+deps="libgsoap-kopano-2-8 libkcutil0 libkcmapi0 libmapi1 libkcfreebusy0 libkcsoapclient0 libkcssl0 libkcsync0 kopano-lang kopano-client kopano-contacts libkcicalmapi0 libvmime-kopano0 libkcinetmapi0 php5-mapi"
+echo "webapp dependencies - $outdir/kopano-webapp/packages"
 for pkg in $deps; do
-	find $outdir/core -name "$pkg"*.deb -exec cp {} $outdir/webapp-dependencies \;
+	find $outdir/kopano-core/packages -name "$pkg"*.deb -exec cp {} $outdir/kopano-webapp/packages \;
+done
+
+# copying over kopano4ucs dependencies
+deps="kopano4ucs-lib kopano4ucs-webapp"
+for pkg in $deps; do
+  cp $outdir/kopano4ucs/"$pkg"*.deb $outdir/kopano-webapp/packages/
 done
 
 # webmeetings
-osc_get kopano4ucs meetings4ucs $outdir/webmeetings
-osc_get kopano4ucs mod_proxy_wstunnel $outdir/webmeetings
-osc_get webmeetings:final kopano-webmeetings $outdir/webmeetings
-osc_get webmeetings:final kopano-webapp-plugin-meetings $outdir/webmeetings
+osc_get kopano4ucs meetings4ucs $outdir/meetings4ucs/packages
+osc_get kopano4ucs mod_proxy_wstunnel $outdir/meetings4ucs/packages
+osc_get webmeetings:final kopano-webmeetings $outdir/meetings4ucs/packages
+osc_get webmeetings:final kopano-webapp-plugin-meetings $outdir/meetings4ucs/packages
 
 # z-push
-osc_get z-push:pre-final z-push $outdir/z-push
+osc_get z-push:pre-final z-push $outdir/z-push-kopano/packages
 
 # remove source and other files
-#find $outdir -type f ! -name "*.deb" -delete
+cd $outdir
+outdirs="$outdir/kopano-core/packages $outdir/kopano-webapp/packages $outdir/meetings4ucs/packages $outdir/z-push-kopano/packages"
+for finaldir in $(find . -mindepth 1 -maxdepth 1 -type d); do
+	[[ $finaldir = ./kopano4ucs ]] && continue
+	if [ ! -e $(basename $finaldir).tar.gz ]; then
+		#find $finaldir/packages -type f ! -name "*.deb" -delete
+		tar --exclude='*.gz' --exclude='*.dsc' --exclude='*.changes' --exclude='_buildenv' --exclude='_statistics' \
+		-czvf $(basename $finaldir).tar.gz $finaldir
+	fi
+done
+
