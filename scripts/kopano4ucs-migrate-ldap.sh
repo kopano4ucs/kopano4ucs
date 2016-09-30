b4f6add75a0822b395a3e150d107eda6f72f4df6
Felix Bartels <felix@host-consultants.de>
2016-09-30 18:42:48 +0200

    KUCS-2 moved commits for migration script to separate branch
    KUCS-2 forgot exec permissions
    
    KUCS-2 optimize migrate-ucr a bit, first shot at migrate-files
    
    KUCS-2 fixes for migrate-files
    
    kopano4ucs-migrate-zarafa adaptions:
    
    * print information how to start the remaining services at the end of
      migration
    * Use ldap.secret file directly with -y for ldap* calls
    * Update migrate-ucr script
    
    actually remove files after rsync
    
    need more quotes
    
    KUCS-2 migrate mysql secret as zarafadbuser needs its password to connect
    
    KUCS-2 migrate attachments last
    
    KUCS-2 make a logfile of the migration
    
    KUCS-2 add a message to the general migration script asking for confirmation
    
    KUCS-2 fix test
    
    KUCS-2 explicitly set bash
    
    KUCS-2 tee should append
    
    KUCS-2 log should go to /var/log
    
    KUCS-2 include zarafa schema in kopano migration package. loose dependency on schema packages
    
    KUCS-2 add separator to logging, fix attachments not being migrated
    
    KUCS-2 adapt ldap script to also be capable of running on slave systems
    
    ZUCS-2 fix file migration
    
    apply patch from Univention
    
    zucs-2 minor formatting. keep ldif files after script finishes
    
    apply patch from univention for exit function
    
    KUCS-2 fail migration script if ldap fails
    
    KUCS-2 dont restrict edits to cn=users (for contacts, shared-resources, groups)
    
    KUCS-2 make erroring more obvious in the script
    
    KUCS-2 fix rsync

---
+++ scripts/kopano4ucs-migrate-ldap.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LDAP_HOST="$(ucr get ldap/master)"
+LDAP_PORT="$(ucr get ldap/master/port)"
+LDAP_USER="cn=admin,$(ucr get ldap/base)"
+LDAP_PASSWD_FILE="/etc/ldap.secret"
+LDAP_LDIF="/tmp/zarafa2kopano.ldif"
+
+cleanup (){
+	[ -e /tmp/zarafa2kopano.passwd ] && rm /tmp/zarafa2kopano.passwd
+}
+
+if [ ! -e $LDAP_PASSWD_FILE ]; then
+	echo "It seems you are running this script on an Univention Slave installation."
+	read -e -p "Please enter password of your domain Administrator: " LDAP_PASSWD_TEMP
+	LDAP_USER="uid=Administrator,cn=users,$(ucr get ldap/base)"
+	echo -n $LDAP_PASSWD_TEMP > /tmp/zarafa2kopano.passwd
+	LDAP_PASSWD_FILE="/tmp/zarafa2kopano.passwd"
+	chmod go-rw $LDAP_PASSWD_FILE
+fi
+
+# connection test
+ldapsearch -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -b "$(ucr get ldap/base)" -y "$LDAP_PASSWD_FILE" cn=admin > /dev/null || { cleanup; exit 1; }
+
+grep ^attributetype /usr/share/kopano4ucs-schema/zarafa4ucs.schema -A1 | grep NAME | sed -e 's#.*NAME..##g' -e 's#.$##' | while read line; do
+	new_attrib="`echo $line | sed 's#zarafa#kopano#'`"
+	ldapsearch -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -b "$(ucr get ldap/base)" -y "$LDAP_PASSWD_FILE" -o ldif-wrap=no -LLL -x "$line=*" dn "$line" | \
+	sed "s#$line: \(.*\)#changetype: modify\nadd: $new_attrib\n$new_attrib: \1\n-\ndelete: $line\n$line: \1\n-#" >> "$LDAP_LDIF".step2_attribute_data
+done
+
+
+# We now have the LDIF, we now need to extend the objects with the new objectClass.
+grep ^dn "$LDAP_LDIF".step2_attribute_data | sort | uniq | sed 's#dn: ##' | while read dn; do
+	ldapsearch -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -b "$(ucr get ldap/base)" -y "$LDAP_PASSWD_FILE" -o ldif-wrap=no -LLL -x -s base -b "$dn" objectclass | \
+	egrep '(^dn|zarafa)' | sed -e '/^objectClass/ s#zarafa#kopano#' | sed 's#objectClass:\(.*\)#changetype: modify\nadd: objectClass\nobjectClass:\1\n#'
+done >> "$LDAP_LDIF".step1_objectclass_extend
+ldapmodify -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -y "$LDAP_PASSWD_FILE" -f "$LDAP_LDIF".step1_objectclass_extend
+
+# We are done with the LDIF, now lets do it.
+ldapmodify -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -y "$LDAP_PASSWD_FILE" -f "$LDAP_LDIF".step2_attribute_data
+
+grep ^dn "$LDAP_LDIF".step2_attribute_data | sort | uniq | sed 's#dn: ##' | while read dn; do
+	ldapsearch -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -b "$(ucr get ldap/base)" -y "$LDAP_PASSWD_FILE" -o ldif-wrap=no -LLL -x -s base -b "$dn" objectclass | \
+	egrep '(^dn|zarafa)' | sed 's#objectClass:\(.*\)#changetype: modify\ndelete: objectClass\nobjectClass:\1\n#'
+done >> "$LDAP_LDIF".step3_objectclass_remove
+ldapmodify -h "$LDAP_HOST" -p "$LDAP_PORT" -D "$LDAP_USER" -y "$LDAP_PASSWD_FILE" -f "$LDAP_LDIF".step3_objectclass_remove
+
+# Clean up
+cleanup
