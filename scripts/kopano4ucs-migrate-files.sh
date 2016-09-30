ad37585151ff5674eacf6a54736e6c67de3bbd04
Felix Bartels <felix@host-consultants.de>
2016-09-30 21:18:08 +0200

    KUCS-2 optimize migrate-ucr a bit, first shot at migrate-files

--- scripts/kopano4ucs-migrate-files.sh
+++ scripts/kopano4ucs-migrate-files.sh
@@ -5,6 +5,38 @@ if [ -e /etc/mapi/zarafa.inf ]; then
 	rm /etc/mapi/zarafa.inf
 fi
 
-# check if attachment_path or index_path are defined as an ucr variable
-# if not defined set variable with /var/lib/zarafa/$
-# chown in both cases
+# check if /var/lib/zarafa is a mount. if so keep everything there
+if [ ! -z $(mount | grep /var/lib/zarafa) ]; then
+	echo "/var/lib/zarafa is a mount, going to reuse it"
+	basedirectory=/var/lib/zarafa
+	olddir=true
+else
+	basedirectory=/var/lib/kopano
+	olddir=false
+fi
+
+# helper function to set variable, rsync & chown contents
+function migratedir() {
+	registry="$1"
+	directory="$2"
+	olddir="$3"
+
+	# if not already defined as a variable store location now
+	# (assuming that the default location is used otherwise)
+	if [ -z $(ucr get "$registry") ]; then
+		ucr set "$registry"="$directory"
+	fi
+
+	if [ $olddir == "false" ]; then
+		rsync -avP $(basename $directory) "$directory"
+	fi
+	chown -R kopano:kopano "$directory"
+}
+
+
+migratedir kopano/cfg/server/attachment_path $basedirectory/attachments $olddir
+migratedir kopano/cfg/server/client_update_path $basedirectory/client $olddir
+
+# deleting old search index
+rm -r $basedirectory/search/
+migratedir kopano/cfg/search/index_path $basedirectory/search/
--- scripts/kopano4ucs-migrate-ucr.sh
+++ scripts/kopano4ucs-migrate-ucr.sh
@@ -6,36 +6,36 @@ WORKDIR=$(mktemp -d)
 ucr dump > $WORKDIR/UCR_DUMP
 
 # extract migration relevant ucr variables
-grep 'zarafa/cfg\|zarafa/default\|zarafa/webapp' $WORKDIR/UCR_DUMP > $WORKDIR/ZARAFA-values
+grep 'zarafa/cfg\|zarafa/default\|zarafa/webapp' $WORKDIR/UCR_DUMP > \
 
 # remove ucr variables that should not be migrated
-sed -i '/ldap_bind_/d' $WORKDIR/ZARAFA-values
-sed -i '/license_socket/d' $WORKDIR/ZARAFA-values
-sed -i '/local_admin_users/d' $WORKDIR/ZARAFA-values
-sed -i '/pid_file/d' $WORKDIR/ZARAFA-values
-sed -i '/run_as_group/d' $WORKDIR/ZARAFA-values
-sed -i '/run_as_user/d' $WORKDIR/ZARAFA-values
-sed -i '/search_socket/d' $WORKDIR/ZARAFA-values
-sed -i '/server_bind_name/d' $WORKDIR/ZARAFA-values
-sed -i '/server_pipe_name/d' $WORKDIR/ZARAFA-values
-sed -i '/server_pipe_priority/d' $WORKDIR/ZARAFA-values
-sed -i '/server_socket/d' $WORKDIR/ZARAFA-values
+sed -i '/ldap_bind_/d' \
+    -i '/license_socket/d' \
+    -i '/local_admin_users/d' \
+    -i '/pid_file/d' \
+    -i '/run_as_group/d' \
+    -i '/run_as_user/d' \
+    -i '/search_socket/d' \
+    -i '/server_bind_name/d' \
+    -i '/server_pipe_name/d' \
+    -i '/server_pipe_priority/d' \
+    -i '/server_socket/d' $WORKDIR/ZARAFA-values
 # remove default value for mysql_password
 sed -i '\:/etc/zarafa-mysql.secret:d' $WORKDIR/ZARAFA-values
 
 # delete ucr for default certificates (new default ones are already generated for kopano)
 # if admin has set custom certificates over ucr this value will be migrated
-sed -i '\:/etc/zarafa/ssl/cert.pem:d' $WORKDIR/ZARAFA-values
-sed -i '\:/etc/zarafa/ssl/private.key:d' $WORKDIR/ZARAFA-values
-sed -i '\:/etc/zarafa/ssl/server.pem:d' $WORKDIR/ZARAFA-values
+sed -i '\:/etc/zarafa/ssl/cert.pem:d' \
+    -i '\:/etc/zarafa/ssl/private.key:d' \
+    -i '\:/etc/zarafa/ssl/server.pem:d' $WORKDIR/ZARAFA-values
 
-# replace occurence of zarafa with sed (because of changed ldap objectclasses and attributes
-sed -i 's/zarafaAccount/kopanoAccount/' $WORKDIR/ZARAFA-values
-sed -i 's/zarafa-group/kopano-group/' $WORKDIR/ZARAFA-values
-sed -i 's/zarafaSharedStoreOnly/kopanoSharedStoreOnly/' $WORKDIR/ZARAFA-values
-sed -i 's/zarafa-user/kopano-user/' $WORKDIR/ZARAFA-values
-sed -i 's/ZARAFA_LOCALE/KOPANO_LOCALE/' $WORKDIR/ZARAFA-values
-sed -i 's/ZARAFA_USERSCRIPT_LOCALE/KOPANO_USERSCRIPT_LOCALE/' $WORKDIR/ZARAFA-values
+# replace occurence of zarafa with     (because of changed ldap objectclasses and attributes
+sed -i 's/zarafaAccount/kopanoAccount/' \
+    -i 's/zarafa-group/kopano-group/' \
+    -i 's/zarafaSharedStoreOnly/kopanoSharedStoreOnly/' \
+    -i 's/zarafa-user/kopano-user/' \
+    -i 's/ZARAFA_LOCALE/KOPANO_LOCALE/' \
+    -i 's/ZARAFA_USERSCRIPT_LOCALE/KOPANO_USERSCRIPT_LOCALE/' $WORKDIR/ZARAFA-values
 
 # replace zarafa in ucr path with kopano for all that remains
 sed -i 's/^zarafa/kopano/' $WORKDIR/ZARAFA-values
@@ -46,6 +46,6 @@ while read line; do
 	value=${line#*:}
 	# $value comes with a leading space, so we still have to remove it
 	ucr set $key="${value#"${value%%[![:space:]]*}"}"
-done < $WORKDIR/ZARAFA-values
+done < \
 
 rm -rf $WORKDIR
